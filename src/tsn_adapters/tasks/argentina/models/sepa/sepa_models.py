7c5a28e4cb48c42a9b2d6cfe7316b186b24ef055
Raffael Campos <raffael.rcampos@gmail.com>
2025-01-14 13:45:18 -0300

    feat(argentina): refactor SEPA data models and enhance ingestion processing (#43)
    - Removed the `FullSepaProductosDataModel` and introduced `SepaProductosAlternativeModel` to support alternative data structures for SEPA products.
    - Enhanced `SepaProductosDataModel` and added `SepaProductosDataModelAlt1` for improved data handling and conversion capabilities.
    - Updated `resource_processor.py` to utilize the new alternative models, allowing for flexible data ingestion based on available columns.
    - Introduced comprehensive unit tests for the new models and their functionalities, ensuring data integrity and validation.
    - Improved logging and error handling in the ingestion process to enhance traceability and debugging.

--- src/tsn_adapters/tasks/argentina/models/sepa/sepa_models.py
+++ src/tsn_adapters/tasks/argentina/models/sepa/sepa_models.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
-from typing import TypeVar
+from io import StringIO
+from logging import Logger
+from typing import TypeVar, cast
 
+import pandas as pd
 import pandera as pa
 from pandera.typing import DataFrame, Series
+from pydantic import BaseModel
 
 from tsn_adapters.utils.filter_failures import filter_failures
 
@@ -11,7 +15,70 @@ T = TypeVar("T", bound="ProductDescriptionModel")
 S = TypeVar("S", bound="SepaProductosDataModel")
 
 
-class SepaProductosDataModel(pa.DataFrameModel):
+class SepaProductosAlternativeModel(type(BaseModel), pa.DataFrameModel):
+    """
+    Alternative model for SEPA productos data.
+    """
+
+    @classmethod
+    @abstractmethod
+    def get_differentiation_columns(cls) -> list[str]:
+        """
+        The columns that differentiate the alternative model from the core model.
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def to_core_model(cls, df: DataFrame[T]) -> DataFrame["SepaProductosDataModel"]:
+        """
+        Convert the alternative model to the core model.
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def has_columns(cls, header: str) -> bool:
+        """
+        Check if the header has the columns of the current class.
+        """
+        return all(column in header for column in cls.get_differentiation_columns())
+
+    @classmethod
+    def from_csv(
+        cls,
+        date: str,
+        lines: list[str],
+        logger: Logger,
+    ) -> DataFrame[T]:
+        """
+        Read the CSV file and return the core model.
+        """
+
+        text_content = "\n".join(lines)
+        content_io = StringIO(text_content)
+
+        try:
+            df = pd.read_csv(
+                content_io,
+                skip_blank_lines=True,
+                sep="|",
+                usecols=cls.get_differentiation_columns(),
+            )
+        except Exception as e:
+            # if the error is about missing columns, print first 2 lines
+            if "Usecols do not match columns, columns expected but not found" in str(e):
+                logger.error(f"First 2 lines of {content_io}:\n{lines[:2]}")
+
+            raise e
+
+        df["date"] = date
+        original_len = len(df)
+        df = filter_failures(df, cls)
+        if len(df) < original_len:
+            logger.warning(f"Filtered out {original_len - len(df)} invalid rows")
+        return cast(DataFrame[T], df)
+
+
+class SepaProductosDataModel(SepaProductosAlternativeModel):
     """
     Pandera model for core SEPA productos data.
     """
@@ -26,47 +93,52 @@ class SepaProductosDataModel(pa.DataFrameModel):
         strict = "filter"
 
     @classmethod
-    def from_full_data(
-        cls: type[S],
-        data: DataFrame["FullSepaProductosDataModel"],
-    ) -> DataFrame[S]:
-        df = data[
-            [
-                "id_producto",
-                "productos_descripcion",
-                "productos_precio_lista",
-                "date",
-            ]
-        ]
-        df = filter_failures(df, cls)
-        return df
+    def get_differentiation_columns(cls) -> list[str]:
+        return ["productos_precio_lista", "productos_descripcion", "id_producto"]
+
+    @classmethod
+    def to_core_model(cls, df: DataFrame[T]) -> DataFrame["SepaProductosDataModel"]:
+        """
+        Convert the alternative model to the core model.
+        """
+        return cast(DataFrame[SepaProductosDataModel], df)
 
 
-class FullSepaProductosDataModel(SepaProductosDataModel):
+# ALTERNATIVE MODELS
+
+
+class SepaProductosDataModelAlt1(SepaProductosAlternativeModel):
     """
-    Pandera model for the full SEPA productos data, including additional details.
+    Alternative model for SEPA productos data.
     """
 
-    id_comercio: Series[str]
-    id_bandera: Series[str]
-    id_sucursal: Series[str]
-    productos_ean: Series[str]
-    productos_cantidad_presentacion: Series[float]
-    productos_unidad_medida_presentacion: Series[str] = pa.Field(nullable=True)
-    productos_marca: Series[str] = pa.Field(nullable=True)
-    productos_precio_referencia: Series[float]
-    productos_cantidad_referencia: Series[float]
-    productos_unidad_medida_referencia: Series[str] = pa.Field(nullable=True)
-    productos_precio_unitario_promo1: Series[float] = pa.Field(nullable=True)
-    productos_leyenda_promo1: Series[str] = pa.Field(nullable=True)
-    productos_precio_unitario_promo2: Series[float] = pa.Field(nullable=True)
-    productos_leyenda_promo2: Series[str] = pa.Field(nullable=True)
+    id_producto: Series[str]
+    productos_descripcion: Series[str]
+    precio_unitario_bulto_por_unidad_venta_con_iva: Series[float]
+    date: Series[str]
 
     class Config(SepaProductosDataModel.Config):
         strict = True
         coerce = True
         add_missing_columns = False
 
+    @classmethod
+    def get_differentiation_columns(cls) -> list[str]:
+        return ["precio_unitario_bulto_por_unidad_venta_con_iva", "productos_descripcion", "id_producto"]
+
+    @classmethod
+    def to_core_model(cls, df: DataFrame[T]) -> DataFrame["SepaProductosDataModel"]:
+        new_df = pd.DataFrame(
+            {
+                "id_producto": df.id_producto,
+                "productos_descripcion": df.productos_descripcion,
+                "productos_precio_lista": df.precio_unitario_bulto_por_unidad_venta_con_iva,
+                "date": df.date,
+            }
+        )
+        return filter_failures(new_df, SepaProductosDataModel)
+
+# Other Models
 
 class ProductDescriptionModel(pa.DataFrameModel):
     """
